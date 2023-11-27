6e56e44fd45b1ab82b923dff8a0301f1b9f2d8f5
Enrique Izel <eleaoizel@gmail.com>
2023-11-27 15:53:35 -0400

    feat: initiate recyclerView

--- app/src/main/java/com/example/waitlistapp/MainActivity.kt
+++ app/src/main/java/com/example/waitlistapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.waitlistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.waitlistapp.adapter.GuestAdapter
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+    recyclerView.layoutManager = LinearLayoutManager(this)
+    recyclerView.adapter = GuestAdapter()
   }
+
+  fun onClickAddGuest(view: View) {}
 }
