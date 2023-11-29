3a845424d70702b0056c87ac3f5113bc6dbc9fc2
Enrique Izel <eleaoizel@gmail.com>
2023-11-29 16:10:01 -0400

    feat: implement function to get guests

--- app/src/main/java/com/example/waitlistapp/MainActivity.kt
+++ app/src/main/java/com/example/waitlistapp/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.waitlistapp
 
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.waitlistapp.adapter.GuestAdapter
+import com.example.waitlistapp.data.WaitlistContract
+import com.example.waitlistapp.data.WaitlistDBHelper
 
 class MainActivity : AppCompatActivity() {
+
+  lateinit var sqliteDatabase: SQLiteDatabase
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    val cursor = getAllGuests()
+    val waitlistDBHelper = WaitlistDBHelper(this)
     val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
     recyclerView.layoutManager = LinearLayoutManager(this)
-    recyclerView.adapter = GuestAdapter()
+    recyclerView.adapter = GuestAdapter(cursor)
+
+    sqliteDatabase = waitlistDBHelper.writableDatabase
+  }
+
+  private fun getAllGuests(): Cursor {
+    return sqliteDatabase.query(
+      WaitlistContract.WaitlistEntry.TABLE_NAME,
+      null,
+      null,
+      null,
+      null,
+      null,
+      WaitlistContract.WaitlistEntry.COLUMN_GUEST_TIMESTAMP
+    )
   }
 
   fun onClickAddGuest(view: View) {}
