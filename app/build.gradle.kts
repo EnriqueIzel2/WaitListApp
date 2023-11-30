a1bd7063ac324192ac354d94075a8f9df6df61b1
Enrique Izel <eleaoizel@gmail.com>
2023-11-30 15:42:41 -0400

    fix: update Sdk number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "com.example.waitlistapp"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.example.waitlistapp"
     minSdk = 30
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 
